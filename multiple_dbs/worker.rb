bc9e6f380e6e76899f7840a12408f059b6cb83cb
Gerhard Schlager <gerhard.schlager@discourse.org>
2024-07-26 23:36:08 +0200

    Revert "with readline"
    This reverts commit 57c9c6780dd05cc6af8bb2267605a3faa7a110f8.

--- multiple_dbs/worker.rb
+++ multiple_dbs/worker.rb
@@ -28,10 +28,8 @@ class Worker
     fork_output_stream.close
     parent_input_stream.close
 
-    # start_input_thread(parent_output_stream, worker_pid)
-    # start_output_thread(fork_input_stream)
-
-    start_thread(fork_input_stream, parent_output_stream, worker_pid)
+    start_input_thread(parent_output_stream, worker_pid)
+    start_output_thread(fork_input_stream)
 
     self
   end
@@ -61,7 +59,7 @@ class Worker
 
         Oj.load(parent_input_stream) do |data|
           @job.run(data)
-          fork_output_stream.write(Oj.dump(stats), "\n")
+          fork_output_stream.write(Oj.dump(stats))
         end
       rescue SignalException
         exit(1)
@@ -71,47 +69,30 @@ class Worker
     end
   end
 
-  def start_thread(input_stream, output_stream, worker_pid)
+  def start_input_thread(output_stream, worker_pid)
     @threads << Thread.new do
-      Thread.current.name = "worker_thread#{@index}"
+      Thread.current.name = "worker_#{@index}_input"
 
       begin
         while (data = @input_queue.pop)
           output_stream.write(Oj.dump(data))
-          @output_queue.push(Oj.load(input_stream.readline))
         end
       ensure
         output_stream.close
         Process.waitpid(worker_pid)
-        input_stream.close
       end
     end
   end
 
-  # def start_input_thread(output_stream, worker_pid)
-  #   @threads << Thread.new do
-  #     Thread.current.name = "worker_#{@index}_input"
-  #
-  #     begin
-  #       while (data = @input_queue.pop)
-  #         output_stream.write(Oj.dump(data))
-  #       end
-  #     ensure
-  #       output_stream.close
-  #       Process.waitpid(worker_pid)
-  #     end
-  #   end
-  # end
-  #
-  # def start_output_thread(input_stream)
-  #   @threads << Thread.new do
-  #     Thread.current.name = "worker_#{@index}_output"
-  #
-  #     begin
-  #       Oj.load(input_stream) { |data| @output_queue.push(data) }
-  #     ensure
-  #       input_stream.close
-  #     end
-  #   end
-  # end
+  def start_output_thread(input_stream)
+    @threads << Thread.new do
+      Thread.current.name = "worker_#{@index}_output"
+
+      begin
+        Oj.load(input_stream) { |data| @output_queue.push(data) }
+      ensure
+        input_stream.close
+      end
+    end
+  end
 end
